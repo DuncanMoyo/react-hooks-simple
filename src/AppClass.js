cb67b5345d4ed65c3f89ba480201b02b247eb933
Duncan Moyo <duncanfmoyo@gmail.com>
2020-11-01 21:38:33 +0200

    toggle state with useState

--- src/AppClass.js
+++ src/AppClass.js
@@ -1,22 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class App extends Component {
-
   state = {
-    count : 0
-  }
+    count: 0,
+    isOn: false,
+  };
 
   incrementCount = () => {
-    this.setState(prevState => ({
-      count : prevState.count + 1
-    }))
-  }
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  };
+
+  toggleLight = () => {
+    this.setState((prevState) => ({
+      isOn: !prevState.isOn,
+    }));
+  };
 
   render() {
     return (
-      <button onClick={this.incrementCount} >I was clicked {this.state.count} times</button>
-    )
+      <>
+        <h2>Counter</h2>
+        <button onClick={this.incrementCount}>
+          I was clicked {this.state.count} times
+        </button>
+
+        <h2>Toggle Light</h2>
+        <div
+          onClick={this.toggleLight}
+          style={{ height: "50px", width: "50px", background: this.state.isOn ? 'yellow': "grey" }}
+        ></div>
+      </>
+    );
   }
 }
 
-export default App
+export default App;
--- src/AppFunction.js
+++ src/AppFunction.js
@@ -2,13 +2,35 @@ import React, { useState } from "react";
 
 const AppFunction = () => {
   const [count, setCount] = useState(0);
+  const [isOn, setIsOn] = useState(false);
 
   const incrementCount = () => {
-    setCount(prevCount => prevCount + 1);
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const toggleLight = () => {
+    setIsOn((prevIsOn) => !prevIsOn);
   };
 
   return (
-    <button onClick={incrementCount}> I've been clicked {count} times</button>
+    <>
+      <h2>Counter</h2>
+      <button onClick={incrementCount}> I've been clicked {count} times</button>
+
+      <h2>Toggle Light</h2>
+      <img
+        src={
+          isOn
+            ? "https://icon.now.sh/highlight/fd0"
+            : "https://icon.now.sh/highlight/aaa"
+        }
+        onClick={toggleLight}
+        style={{
+          height: "50px",
+          width: "50px",
+        }}
+      />
+    </>
   );
 };
 
